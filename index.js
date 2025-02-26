2800454062907071c0a1824db101627ed6008f18
Nobz27 <kayondonorbert@icloud.com>
2025-02-26 11:14:26 +0300

    introduction to javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const teachers = [ "Albert" ,"Kevin" ,"Brigid " ,"Isaiah" , "Nigel"];
+for (teacher in teachers){
+    // alert("hello" + teachers[teacher] + ". Welcome to our masterclass.");
+    console.log("hello" + teachers[teacher] + ". Welcome to our masterclass.");
+}
---
+++ variables.js/index.js
@@ -0,0 +1,23 @@
+// Variables allow storing data in memory for use in a program/software
+// In js we create/initialize variables using var, const and let keywords
+
+// var -- old/legacy way of creating variables
+var age = 90
+
+// let and const are modern/es6 ways of creating variables
+let username = "albert"
+const interestRate = 20
+
+// let is used with values/variables that change as the program runs
+// const is used for values/variables that do not change at any point in the life of a program/software eg PI
+const PI = 3.142
+
+// the values in variables can be access in the application using the variable names
+//console.log(something) is a way for developers to print/see outcome on the terminal/console.
+console.log(age)
+console.log(PI + interestRate)
+
+## Assigments 
+
+
+- dicuss variable naming rules and variable naming conventions in js
